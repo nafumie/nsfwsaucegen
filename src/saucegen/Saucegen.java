4765349da3aeb869b8c30b99979db1891d167d0a
Jariel Que <53419401+tanyufukazawa@users.noreply.github.com>
2021-05-06 19:13:58 +0800

    update int generator to reduce chances of 404 not found

--- src/saucegen/Saucegen.java
+++ src/saucegen/Saucegen.java
@@ -9,12 +9,12 @@ public class Saucegen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int min = 1;
-        int max = 200000;
-        //Generate random int value from 1 to 200000
+        int min = 10000;
+        int max = 299999;
+        //Generate random int value from 10000 to 200000
         int random_int = (int) (Math.random() * (max - min + 1) + min);
         System.out.println("Sending you to sauce: " + random_int);
-        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) { // DOES NOT OPEN IN INCOGNITO
             try {
                 Desktop.getDesktop().browse(new URI("https://nhentai.to/g/" + random_int));
             } catch (Exception e) {
